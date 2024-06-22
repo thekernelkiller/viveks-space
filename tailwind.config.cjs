9a909686b3576895839522998975b681d2fd60a9
Vivekananda Sai <v288399.work@gmail.com>
2024-06-22 23:44:01 +0530

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				brand: "rgb(3, 7, 18)"
+				brand: "rgb(23, 37, 84)"
 			},
 			// fontFamily: {
 			// 	serif: ['Times New Roman', 'serif', 'serif-bold'],

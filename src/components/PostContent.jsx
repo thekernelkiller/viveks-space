8b9708ecaea76c8db27e2fd938fdee33fa7916b4
thekernelkiller <v288399.work@gmail.com>
2024-06-27 18:09:14 +0530

    updated home page to show excerpts

--- src/components/PostContent.jsx
+++ src/components/PostContent.jsx
@@ -1,7 +1,7 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
-const PostContent = ({ post }) => {
+const PostContent = ({ post, showExcerpt }) => {
   const { content, title, slug, createdAt } = post;
 
   const dateString = new Date(createdAt).toLocaleDateString("en-US", {
@@ -14,9 +14,13 @@ const PostContent = ({ post }) => {
 
   return (
     <>
-      <h2 className="text-2xl text-center font-bold mb-3">{title}</h2>
-      <ReactMarkdown className="prose md:prose-md" remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown> 
-      <time dateTime="{createdAt}">
+      {!showExcerpt && (
+        <>
+          <h2 className="text-2xl text-center font-bold mb-3">{title}</h2>
+          <ReactMarkdown className="prose md:prose-md" remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+        </>
+      )}
+      <time dateTime={createdAt}>
         <a href={`/posts/${slug}`} className="text-md text-center underline">
           {dateString} at {timeString}
         </a>
@@ -26,3 +30,32 @@ const PostContent = ({ post }) => {
 };
 
 export default PostContent;
+
+// import ReactMarkdown from 'react-markdown';
+// import remarkGfm from 'remark-gfm';
+
+// const PostContent = ({ post }) => {
+//   const { content, title, slug, createdAt } = post;
+
+//   const dateString = new Date(createdAt).toLocaleDateString("en-US", {
+//     dateStyle: "full",
+//   });
+//   const timeString = new Date(createdAt).toLocaleTimeString("en-US", {
+//     timeStyle: "short",
+//     hour12: false
+//   });
+
+//   return (
+//     <>
+//       <h2 className="text-2xl text-center font-bold mb-3">{title}</h2>
+//       <ReactMarkdown className="prose md:prose-md" remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown> 
+//       <time dateTime="{createdAt}">
+//         <a href={`/posts/${slug}`} className="text-md text-center underline">
+//           {dateString} at {timeString}
+//         </a>
+//       </time>
+//     </>
+//   );
+// };
+
+// export default PostContent;
